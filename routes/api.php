697894b01f2b4772cdd404ad689904da61ff7762
gustav <gustav@nerdsofsweden.se>
2019-12-11 23:20:52 +0100

    testa

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 echo 'blue moooon';
 echo 'some days are diamond';
+echo 'Chicken and the egg. What happends tomrrow, trallala';
