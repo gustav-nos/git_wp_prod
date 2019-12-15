9b665d43d54e6bf6d0098fd9088cbe5a12122da6
gustav <gustav@nerdsofsweden.se>
2019-12-15 22:30:47 +0100

    Changed back from clean, to ugly commit.

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,3 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-echo 'blue moooon';
-echo 'some days are diamond';
-echo 'Chicken and the egg. What happends tomrrow, trallala';
